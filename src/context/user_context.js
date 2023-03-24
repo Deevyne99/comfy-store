ab3a839736049b7606099320b76b4afa719f5302
unknown <kaludivine545@gmail.com>
2023-03-24 10:52:27 +0530

    first commit

---
+++ src/context/user_context.js
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
+const UserContext = React.createContext()
+export const UserProvider = ({ children }) => {
+  const [myUser, setMyUser] = useState(null)
+  const { isAuthenticated, user, isLoading, loginWithRedirect, logout } =
+    useAuth0()
+  useEffect(() => {
+    // console.log(`user: ${user}`)
+    // console.log(`isAuthenticated : ${isAuthenticated}`)
+    // console.log(`isLoading :${isLoading}`)
+    if (user) {
+      setMyUser(user)
+    } else {
+      setMyUser(false)
+    }
+  }, [isAuthenticated])
+  return (
+    <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+// make sure use
+export const useUserContext = () => {
+  return useContext(UserContext)
+}
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+import { ProductsProvider } from './context/products_context'
+import { FilterProvider } from './context/filter_context'
+import { CartProvider } from './context/cart_context'
+import { UserProvider } from './context/user_context'
+import { Auth0Provider } from '@auth0/auth0-react'
+// dev-jsxvk66zheufr8es.us.auth0.com
+//NzfQK7BlzgCRVyIdxP4VsCfpy5xyn1qO
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <Auth0Provider
+    domain='dev-jsxvk66zheufr8es.us.auth0.com'
+    clientId='JrQ4QHV9ycRigEb8eNikou42Ah2LEzIp'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
+)
